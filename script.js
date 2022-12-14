576f9b66401f207b70882f0fe4b564514d607459
Toshiro <toshiro3457@gmail.com>
2022-12-13 22:39:36 -0500

    working on setBoardHoverClass

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
-const cellElement = document.querySelectorAll('[data-cell')
+const x_class = "x";
+const circle_class = "circle";
+const cellElement = document.querySelectorAll("[data-cell");
+const board = document.getElementById("board");
+let circleTurn;
 
-cellElement.forEach(cell => {
-  cell.addEventListener("click", handleClick, {once: true})
-};
+//    eventlistener for clicking the tiles   //
 
+startGame();
 
+function startGame() {
+  circleTurn = false;
+  cellElement.forEach((cell) => {
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHoverClass();
+}
+
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = circleTurn ? circle_class : x_class;
+  placeMark(cell, currentClass);
+  swapTurns();
+  setBoardHoverClass();
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(x_class);
+  board.classList.remove(circle_class);
+
+  if (classTurn) {
+    board.classList.add("circle");
+  } else {
+    board.classList.add("x");
+  }
+}
